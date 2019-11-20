5fb0477edf4f9b8c2ca8792d6c6edfd8759b90c7
low-key <tsemibhutia@gmail.com>
2019-11-20 16:53:11 +0530

    Updates

---
+++ precache-manifest.4c3bf887108af3bbfc91ef0f9d2b5fad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaf99cebf5213a0258989c02efe4ac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60e098ce3bf678194259",
+    "url": "/static/css/main.c9bc44f0.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "60e098ce3bf678194259",
+    "url": "/static/js/main.f6993b9e.chunk.js"
+  },
+  {
+    "revision": "50cb9aa5f15f329e71d6",
+    "url": "/static/js/runtime-main.576dec9c.js"
+  }
+]);
